cfddca63f15ff4637a7400f2346ddd96998f6e51
Alejandro Such <alejandro.such@gmail.com>
2016-11-12 12:34:32 +0100

    added reverse fix

--- src/app/registration-form/same-as.validator.ts
+++ src/app/registration-form/same-as.validator.ts
@@ -1,6 +1,9 @@
 import { Directive, forwardRef, Attribute } from '@angular/core';
 import { NG_VALIDATORS, Validator, AbstractControl } from '@angular/forms';
 
+const VALIDATOR_KEY = 'sameAs';
+const VALIDATOR_ERROR = {[VALIDATOR_KEY]: false};
+
 @Directive({
   selector: '[sameAs][ngModel]',
   providers: [
@@ -9,15 +12,31 @@ import { NG_VALIDATORS, Validator, AbstractControl } from '@angular/forms';
 })
 export class SameAsValidator implements Validator {
 
-  constructor(@Attribute('sameAs')public sameAs:string) { }
+  constructor(@Attribute('sameAs')public sameAs:string, @Attribute('reverse')public reverse:boolean=false) { }
 
-  validate(c: AbstractControl): {[key: string]: any} {
-    let areEquals = c.value === c.root.get(this.sameAs).value;
+  validate(control: AbstractControl): {[key: string]: any} {
+    let comparedControl = control.root.get(this.sameAs);
+
+    if(!comparedControl) {
+      return null;
+    }
+
+    let areEquals = control.value === comparedControl.value;
 
     console.log('sameAs validation result is', areEquals);
 
-    if(!areEquals) {
-      return {"sameAs": false};
+    if(!this.reverse && !areEquals) {
+        return VALIDATOR_ERROR;
+    } else if(this.reverse) {
+      if(areEquals) {
+        delete comparedControl.errors[VALIDATOR_KEY];
+
+        if (!Object.keys(comparedControl.errors).length){ 
+          comparedControl.setErrors(null);
+        }
+      } else {
+        comparedControl.setErrors(VALIDATOR_ERROR);
+      }
     }
 
     return null;

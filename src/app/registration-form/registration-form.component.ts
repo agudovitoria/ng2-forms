0acb4d40282efdf38b1137573ed367ee5c8731a1
Alejandro Such <alejandro.such@gmail.com>
2016-11-25 13:04:04 +0100

    Reactive forms. Declaration and submit

--- src/app/registration-form/registration-form.component.ts
+++ src/app/registration-form/registration-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { IFormData } from './IFormData';
 
 @Component({
   selector: 'app-registration-form',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration-form.component.css']
 })
 export class RegistrationFormComponent implements OnInit {
+  formData: FormGroup;
 
-  constructor() { }
+  constructor() { 
 
-  ngOnInit() {
   }
 
+  ngOnInit() {
+    console.log('initializing...');
+
+    this.formData = new FormGroup({
+      name: new FormControl('Perico'),
+      lastName: new FormControl('Palotes'),
+      email: new FormControl(''),
+      gender: new FormControl(''),
+      dob: new FormControl(''),
+      phone: new FormControl(''),
+      address: new FormGroup({
+        addressLine1: new FormControl(''),
+        addressLine2: new FormControl(''),
+        city: new FormControl(''),
+        province: new FormControl(''),
+        zip: new FormControl('')
+      })
+    });
+  }
+
+  onSubmit({ value, valid }: { value: IFormData, valid: boolean }) {
+    console.log(value, valid);
+  }
 }

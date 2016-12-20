ea221142478dc9ccba4ad39770d7a33cc3c177e3
Alejandro Such <alejandro.such@gmail.com>
2016-12-20 08:35:09 +0100

    Using FormBuilder

--- src/app/registration-form/registration-form.component.ts
+++ src/app/registration-form/registration-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, AbstractControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, AbstractControl, FormGroup, Validators } from '@angular/forms';
 import { IFormData } from './IFormData';
 import { RegistrationFormValidators } from './registration-form.validator';
 
@@ -11,27 +11,25 @@ import { RegistrationFormValidators } from './registration-form.validator';
 export class RegistrationFormComponent implements OnInit {
   formData: FormGroup;
 
-  constructor() { 
+  constructor(private fb:FormBuilder) { 
 
   }
 
   ngOnInit() {
-    console.log('initializing...');
-
-    this.formData = new FormGroup({
-      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-      lastName: new FormControl('', ),
-      email: new FormControl('', [Validators.required, , RegistrationFormValidators.email, RegistrationFormValidators.sameAs('email2', true)]),
-      email2: new FormControl('', [Validators.required, RegistrationFormValidators.email, RegistrationFormValidators.sameAs('email', false)]),
-      gender: new FormControl('', Validators.required),
-      dob: new FormControl('', Validators.required),
-      phone: new FormControl('', Validators.required),
-      address: new FormGroup({
-        addressLine1: new FormControl('', Validators.required),
-        addressLine2: new FormControl(''),
-        city: new FormControl('', Validators.required),
-        province: new FormControl('', Validators.required),
-        zip: new FormControl('', Validators.required)
+    this.formData = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', ],
+      email: ['', [Validators.required, RegistrationFormValidators.email, RegistrationFormValidators.sameAs('email2', true)]],
+      email2: ['', [Validators.required, RegistrationFormValidators.email, RegistrationFormValidators.sameAs('email', false)]],
+      gender: ['', Validators.required],
+      dob: ['', Validators.required],
+      phone: ['', Validators.required],
+      address: this.fb.group({
+        addressLine1: ['', Validators.required],
+        addressLine2: [''],
+        city: ['', Validators.required],
+        province: ['', Validators.required],
+        zip: ['', Validators.required]
       })
     });
   }
